b885b1a64393330041ba0b8cde253663c1d5fc40
gustavokubiack <guukubiack@gmail.com>
2022-06-28 03:20:12 -0300

    adding perfil page

---
+++ frontend/src/views/Perfil.vue
@@ -0,0 +1,23 @@
+<template>
+<v-container>
+  <div class="d-flex justify-center">
+  <h1 class="titulo-perfil">Perfil</h1>
+  </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.titulo-perfil{
+  text-align: center;
+  font-family: 'Prata', serif;
+  font-size: 40px;
+  color: #f8f7f2;
+}
+
+</style>
