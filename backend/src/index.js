8a5467fd3ec417a69949aa13612b0d06f77be3cc
gustavokubiack <guukubiack@gmail.com>
2022-07-12 11:44:58 -0300

    deleting auth0-backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,4 @@
 const express = require('express')
-const {auth} = require('express-openid-connect')
-const {requiresAuth} = require ('express-openid-connect')
-require('dotenv').config({ path: '/home/gustavokubiack/merakivoluntariado/backend/.env' });
 
 // Creating an App
 const app = express()
@@ -16,22 +13,6 @@ app.get("/teste", (req, res)=>{
     res.send(('Teste'))
 })
 
-//Auth0
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASEURL,
-    clientID: process.env.CLIENTID,
-    issuerBaseURL: process.env.ISSUERURL
-  };
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
 
 // Listen
 app.listen( port, () => {
