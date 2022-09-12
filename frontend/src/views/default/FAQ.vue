2768369058fce4e2431ea8e2e04503fb019f3275
mariyasmin <mari.yasminnnn@gmail.com>
2022-09-12 13:31:04 -0300

    updating faq page

--- frontend/src/views/default/FAQ.vue
+++ frontend/src/views/default/FAQ.vue
@@ -1,34 +1,35 @@
 <template>
-  <v-container class="pa-8" fluid>
-      <div class="faq">
-    <h1 class="titulo">Perguntas Frequentes</h1> <br> <br>
-    <h2 class="titulo-perguntas">Quem pode se voluntariar em alguma causa?</h2> 
-    <p class="texto-respostas">Qualquer pessoa maior de 18 anos que esteja disposta a realizar o que foi pedido pela instituição.</p> <br>
-    <hr><br>
-    <h2 class="titulo-perguntas">Qual a documentação necessária para me voluntariar?</h2>
-    <p class="texto-respostas">Principalmente RG e CPF, quaisquer outros documentos necessários serão posteriormente requisitados pela instituição escolhida.</p> <br>
-    <hr> <br>
-    
-    <h2 class="titulo-perguntas">Não me identifiquei com o resultado do meu quiz, posso escolher outra causa para me voluntariar?</h2 >
-    <p class="texto-respostas">Claro que sim! O quiz serve como base para redirecionar os usuários para determinada causa, porém, caso já tenha algo em mente ou não se identifique com seu resultado, sinta-se livre para escolher a causa que mais lhe interessa.</p><br> <hr> <br>
+  <v-card>
+    <v-card-actions>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-magnify" }}</v-icon>
+      </v-btn>
+      <v-card-title>
+        <h2 class="titulo-perguntas">
+          Quem pode se voluntariar em alguma causa?
+        </h2>
+      </v-card-title>
+    </v-card-actions>
 
-    <h2 class="titulo-perguntas">As ONGs e Instituições Sociais disponíveis para trabalho voluntário são confiáveis?</h2>  
-    <p class="texto-respostas">Pode ter certeza que sim! O Meraki tem como base a segurança de nossos colaboradores e futuros voluntários. Todas as instituições disponíveis foram passadas por um longo processo seletivo até serem escolhidas para participar de nosso projeto.</p><br> <hr> <br>
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
 
-    <h2 class="titulo-perguntas">Tenho uma Instituição e preciso de voluntários, posso usar o site como meio de consegui-los?</h2>
-    <p class="texto-respostas">Pode sim! Como dito anteriormente, todos os órgãos que fazem parte do Meraki passam por um processo seletivo até serem escolhidos. Caso tenha interesse em colaborar conosco, é só nos contatar pelo e-mail merakivvoluntariado@gmail.com ou pelo nosso telefone, 0800-1234.</p> <br><hr> <br>
-
-    <h2 class="titulo-perguntas">Preciso desembolsar algum valor para poder me voluntariar?</h2>
-    <p class="texto-respostas">Não! Quaisquer possíveis gastos que possam vir a ocorrer durante o voluntariado serão pagos pela instituição de sua escolha.</p> <br><hr> <br>
-
-    <h2 class="titulo-perguntas">Posso me voluntariar em mais de uma causa ao mesmo tempo?</h2>
-    <p class="texto-respostas">Apesar da ajuda sempre ser bem-vinda, cada voluntário pode escolher apenas uma causa por vez para contribuir. Após completar sua missão de voluntariado, sinta-se livre para escolher outra causa e contribuir ainda mais com o mundo em que vivemos! </p><br> <hr> <br>
-  </div>
-  </v-container>
+        <v-card-text class="texto-respostas">
+          Qualquer pessoa maior de 18 anos que esteja disposta a realizar o que
+          foi pedido pela instituição.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
 </script>
 
 <style>
@@ -44,16 +45,17 @@ export default {};
   text-align: justify;
 }
 .titulo-perguntas {
-  font-family: "Prata", serif;
-  color: #f8f7f2;
+  font-family: "Lato", serif;
+  color: #050a30;
+  font-size: 20px;
   font-weight: bold;
 }
-.texto-respostas{
-  color: #f8f7f2;
-  font-size: 20px;
+.texto-respostas {
+  color: #050a30;
+  font-size: 18px;
   font-family: "Lato", serif;
 }
-.faq{
-    text-align: justify;
+.faq {
+  text-align: justify;
 }
 </style>
