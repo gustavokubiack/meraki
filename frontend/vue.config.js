90fea3b9d1ddda6c7037b143cab1c490ba97cc50
gustavokubiack <guukubiack@gmail.com>
2022-11-22 15:35:11 -0300

    upload an image

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,13 +2,4 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-//   devServer: {
-//     proxy:{
-//       '^/api': {
-//         target: 'https://server-meraki.vercel.app',
-//         ws: true,
-//         changeOrigin: true
-//     }, 
-//   },
-// },
 }
